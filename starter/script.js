0cfae7da532666ac310e6436b6bb287d239bddf4
Sweta <aksweta3@gmail.com>
2021-01-29 07:53:26 -0500

    done

---
+++ starter/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+console.log(btnsOpenModal);
+
+
+const openmodal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+
+for(let i=0; i< btnsOpenModal.length;i++)
+btnsOpenModal[i].addEventListener('click',openmodal);
+
+const closemodal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');  
+}
+
+btnCloseModal.addEventListener('click',closemodal);
+overlay.addEventListener('click',closemodal);    
+
+document.addEventListener('keydown', function(e){
+  console.log(e);
+
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+      closemodal();
+  }
+});
